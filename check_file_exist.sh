6c1baf03f3c92e386bd30272742e6603b4ff48ca
salamatou12 <adamouh84@gmail.com>
2025-07-21 22:41:09 +0100

    sala

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-echo "Entrer le nom du fichier :"
+echo "Entrer le nom du fichier: "
 read filename
 if [ -f "$filename" ]; then
     echo "Le fichier '$filename' existe."
 else
     echo "Le fichier '$filename' n'existe pas."
 fi
-exit 0
